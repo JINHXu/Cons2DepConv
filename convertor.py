70bdff674ae36d1854ad1abc7dd362a4309bd2a6
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2020-02-21 22:16:39 +0100

    mark_head

--- convertor.py
+++ convertor.py
@@ -10,8 +10,6 @@
     Honor Code:  I pledge that this program represents my own work.
 """
 
-# hint: not a lot of coding
-
 from dataclasses import dataclass
 from typing import List
 
@@ -73,7 +71,6 @@ def get_label(child, head):
     """
     # FIXME your code goes here
 
-
 def convert(xml_node):
     """Recursively converts the XML nodes corresponding to a sentence from constituency to dependency parsing.
     
@@ -82,19 +79,94 @@ def convert(xml_node):
 
     xml_node : 
         A xml.etree.ElementTree node to convert.
+    
+    Return
+    ------
+    dependency_node :
+        The converted xml_node which is the corresponding node in dependency parsing.
     """
-    # hints: 20 lines or so
-    # finding, for each word in the sentence, the corresponding head word
-    # multiple HD annotations collide, and none of the words are marked as the head by the constituency annotation
 
-    # a bfs? implemented recursively
+    # traverse the XML tree top-dwon and label the head of each constituent by adding atrribute(HEAD): value(HEADID) pairs to each XML node
+    mark_head(xml_node)
 
-    # FIXME your code goes here
-    # traverse the XML tree to construct dependency tree
+    # for each word find its head bottom-up: backtracking from each word node
+    for word_node in xml_node.findall('.//word'):
+        
 
-    sentence_root = Node()
-    # create root node
-    return sentence_root
+
+
+    # build the nodes
+    return
+
+def mark_head(xml_node):
+    """
+    Helper function of convert(): traverse the XML tree top-down and mark the head for each node(comstituent), and mark constituent for later backtracking
+
+    Parameters
+    ----------
+    xml_node : A xml.etree.ElementTree node to start the top-down traversal from.
+    """
+    # not hitting the terminal node
+    if xml_node.tag != 'word':
+        children_HD = []
+        children_nonHD = []
+        
+        # each DIRECT child node of current node
+        for node in xml_node.findall('node'):
+            ## Heuristics of determining the head of current node(this constituent):
+            ## 1. If there is an explicitly marked head within a constituent (HD annotation), the head of the marked constituent is the head
+            ## 2. Otherwise, if there is no head marking but you have LK, use the head of it as the head
+            ## 3. And if none of the above(multiple hd), choose the leftmost element.
+
+            if node.attrib['func'] == "HD":
+                children_HD.append(node)
+            else:
+                if node.attrib['cat'] == 'LK':
+                    children_nonHD.append(node)
+        ## 1.
+        if len(children_HD) == 1:
+            # head of the current node
+            head = children_HD[0]
+            # set the func value of the head node
+            head.attrib['func'] == 'HD'
+            # mark the head of the current node
+            xml_node.set('HEAD', head.attrib['{http://www.w3.org/XML/1998/namespace}id'])
+        
+        ## 2.
+        elif len(children_HD) == 0:
+            no_lkchild = True
+            for child in children_nonHD:
+                if child.attrib['cat'] == 'LK':
+                    no_lkchild = False
+                    # set the func value of the head node
+                    child.attrib['func'] == 'HD'
+                    # mark the head of current node
+                    xml_node.set('HEAD', child.attrib['{http://www.w3.org/XML/1998/namespace}id'])
+                    break
+            if not no_lkchild:
+                # left-most child node is the head
+                head = children_nonHD[0]
+                # set the func value of the head node
+                head.attrib['func'] == 'HD'
+                # mark the head of the current node
+                xml_node.set('HEAD', head.attrib['{http://www.w3.org/XML/1998/namespace}id'])
+        ## 3.
+        else:
+            # left-most child node is the head
+            head = children_HD[0]
+            # set the func value of the head node
+            head.attrib['func'] == 'HD'
+            # mark the head of the current node
+            xml_node.set('HEAD', head.attrib['{http://www.w3.org/XML/1998/namespace}id'])
+        
+        # recursive calls
+        for node in xml_node.findall('node'):
+            mark_head(node)
+    # hitting the leaf node
+    else:
+        return
+        
+
 
 def to_conll(out, sentence_root):
     """Writes a sentence to the specified file in conll-x format.
@@ -117,5 +189,10 @@ if __name__ == '__main__':
     
     with open('data/sample-sentences.conllx', mode='w', encoding='utf8') as out_f:
         for sent in root.findall('.//sentence'):
+            # for node in sent.findall(".//node[@{http://www.w3.org/XML/1998/namespace}id='s8_508']"):
+                # print(node)
+        
+            # print(sent.attrib['{http://www.w3.org/XML/1998/namespace}id'])
+        
             sentence_root = convert(sent)
             to_conll(out_f, sentence_root)
